b02344d5344f37ac5fbeb8ce0c4d6f642f3f988f
Mat Burnham <matburnham@gmail.com>
2019-01-27 19:06:28 -0500

    Add another test for an OGNTRK message containing only a comment

--- tests/parser/test_parse_tracker.py
+++ tests/parser/test_parse_tracker.py
@@ -37,10 +37,11 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(message['noise_level'], -110.5)
         self.assertEqual(message['relays'], 1)
 
-    def test_no_hw_version(self):
+    def test_no_hw_version_position(self):
         # Full live message without hardware version:
         # "ICA38C6FB>APRS,qAS,LFNZ:/101313h4346.25N/00453.67E'355/042/A=001161 !W15! id0538C6FB +1723fpm +0.1rot 20.0dB 0e -8.0kHz gps1x1"
         message = TrackerParser.parse_position("id0538C6FB +1723fpm +0.1rot 20.0dB 0e -8.0kHz gps1x1")
+
         self.assertEqual(message['address_type'], 1)
         self.assertEqual(message['aircraft_type'], 1)
         self.assertFalse(message['stealth'])
@@ -51,6 +52,13 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(message['error_count'], 0)
         self.assertEqual(message['frequency_offset'], -8.0)
         self.assertEqual(message['gps_quality'], {'horizontal': 1, 'vertical': 1})
+
+    def test_no_hw_version_status(self):
+        # OGN0332FC>OGNTRK,qAS,OxfBrtn2:>231801h Pilot=Pawel Base=Barton
+        message = TrackerParser.parse_status(">231801h Pilot=Pawel Base=Barton")
+
+        # Note: the actual comment doesn't appear to be parsed
+        self.assertNotEqual(message, None)
 
 if __name__ == '__main__':
     unittest.main()
